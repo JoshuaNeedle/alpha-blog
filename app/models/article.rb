deb93ef0e8e59ced0ee778e78fd607cc4324e9b1
Joshua Needle <joshua.a.needle@gmail.com>
2017-12-01 21:49:22 +0000

    Created some functionality for my search bar

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,9 @@ class Article < ActiveRecord::Base
   has_many :article_categories
   has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 10 }
   validates :user_id, presence: true
+  def self.search(search)
+    where("title LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%") 
+  end
 end
